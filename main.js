e33572c51234bdd07806aabe053dc922bfe3577d
Jameka Echols <jameka.echols@gmail.com>
2019-03-05 11:57:24 -0600

    finished practice 3

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 console.log('hey, you')
+/*
+createElement()
+JavaScript provides the document.createElement() method which creates a virtual DOM element that doesn't exist in the DOM until you add it.
+
+appendChild()
+The document.appendChild() method will take a virtual DOM element you created and attach it as a child element of another one.
+*/
+
+
+// Create an unordered list element
+const list = document.createElement('ul')
+
+// Create a list item for our list
+const listItem = document.createElement('li')
+listItem.className = "president"
+listItem.textContent = "George Bush"
+
+// Put the list item on the unordered list
+list.appendChild(listItem)
+
+console.log(list)
+
+// 2. In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
+const messages = document.querySelector(".messages") // article tag
+
+// 3. Create five (5) section components, each with a class of message, and with the content of your choosing.
+const messageComp1 = () => {
+    const text = {
+
+    }
+}
